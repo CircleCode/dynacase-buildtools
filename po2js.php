2b2395684860ef15f8d7456db78f0962ab2e842f
Charles Bonnissent <charles.bonnissent@anakeen.com>
2013-02-05 23:26:03 +0100

    Beautification on po2js (refs #3568)

--- po2js.php
+++ po2js.php
@@ -5,17 +5,26 @@
  * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
  */
 
-Class po2js {
+
+// mettre 2 retour chariots en fin de ligne
+$c=new Po2js($argv[1]);
+$c->po2array();
+print $c->po2json();
+
+Class Po2js {
+
     public $pofile="";
     public $entry=array();
     private $encoding='utf-8';
-    function __construct($pofile) {
+
+    public function __construct($pofile) {
         $this->pofile=$pofile;
     }
+
     public function parseEntry(&$out) {
         $out = preg_replace(
-			"/(?m)\[BLOCK\s*([^\]]*)\](.*?)\[ENDBLOCK\s*\\1\]/se", 
-			"\$this->memoEntry('\\1','\\2')",
+            "/(?m)\[BLOCK\s*([^\]]*)\](.*?)\[ENDBLOCK\s*\\1\]/se", 
+            "\$this->memoEntry('\\1','\\2')",
         $out);
     }
     public static function trimquote($s) {
@@ -25,17 +34,17 @@ Class po2js {
     public function memoEntry($key,$text) {
         $tkey=explode("\n",$key);
         $ttext=explode("\n","$text");
-        $key=trim(implode("\n",array_map('po2js::trimquote',$tkey)));
-        $text=trim(implode("\n",array_map('po2js::trimquote',$ttext)));
-        //    print sprintf("msgid=[[[%s]]] msgstr=[[[%s]]]\n", $key, $text);
-        if ($key && $text)  $this->entry[$key]=$text;
+        $key=trim(implode("\n",array_map('Po2js::trimquote',$tkey)));
+        $text=trim(implode("\n",array_map('Po2js::trimquote',$ttext)));
+        if ($key && $text)  {
+            $this->entry[$key]=$text;
+        }
         else if ($key=="") {
             if (stristr($text,"charset=ISO-8859") !== false) {
                 $this->encoding='iso';
             }
         }
     }
-
 
     public function po2array() {
         if (file_exists($this->pofile)) {
@@ -44,29 +53,22 @@ Class po2js {
                 $pocontent.="\n\n";
                 preg_match_all('/^msgid (?P<msgid>".*?)msgstr (?P<msgstr>".*?")\n\n/ms', $pocontent, $matches, PREG_SET_ORDER);
                 foreach($matches as $m) {
-                    //  $msgid = str_replace("\n", " ", $m['msgid']);
-                    //$msgstr = str_replace("\n", " ", $m['msgstr']);
                     $this->memoEntry($m['msgid'], $m['msgstr']);
                 }
             }
         }
-
     }
 
     public function po2json() {
         $this->po2array();
         if (count($this->entry) > 0) {
-        $js=json_encode($this->entry);
-        if ($this->encoding=="iso") $js=utf8_encode($js);
-        return $js;
-        } else return "";
-       
+            $js=json_encode($this->entry);
+            if ($this->encoding === "iso") {
+                $js=utf8_encode($js);
+            }
+            return $js;
+        } else {
+            return "";
+       }
     }
 }
-
-// mettre 2 retour chariots en fin de ligne
-
-$c=new po2js($argv[1]);
-$c->po2array();
-print $c->po2json();
-?>
